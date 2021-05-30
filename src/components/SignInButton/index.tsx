ddf2ecafff7b9114eedad73d3f51493ce1c87439
Kael Henrique da Silva <kael_hs@hotmail.com>
2021-05-30 04:38:13 -0300

    Social authentication witch github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,19 +1,30 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 import styles from './styles.module.scss';
+import { signIn, signOut, useSession } from 'next-auth/client';
 
 export function SignInButton (){
 
-    const isUserLoggedIn = true ;
+    const [ session ] = useSession();
 
-    return isUserLoggedIn ? ( 
-        <button type="button" className={styles.signInButton}>
+    console.log(session);
+
+    return session ? ( 
+        <button 
+            type="button" 
+            className={styles.signInButton}
+            onClick={ () => signOut() }
+            >
         <FaGithub color="#04d361"/>
-            Kael Silva
+            {session.user.name}
             <FiX color="#737380" className={ styles.closeIcon }/>
         </button>
     ) : (
-        <button type="button" className={styles.signInButton}>
+        <button 
+            type="button" 
+            className={styles.signInButton}
+            onClick={ () => signIn('github')} 
+        >
         <FaGithub color="#eba417"/>
         Sign in with Github
         </button>
