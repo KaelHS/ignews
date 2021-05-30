b67a2a5959ca88d0ebf0298df9e2de2fc9de3b36
Kael Henrique da Silva <kael_hs@hotmail.com>
2021-05-30 19:19:26 -0300

    User insert query on database

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
-import NextAuth from 'next-auth'
-import Providers from 'next-auth/providers'
+import NextAuth from 'next-auth';
+import Providers from 'next-auth/providers';
+import { query as q } from 'faunadb';
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   providers: [
@@ -9,4 +12,44 @@ export default NextAuth({
       scope: 'read:user'
     }),
   ],
+  // jwt: {
+  //   signingKey: process.env.SIGNIN_KEY
+  // },
+  callbacks: {
+    async signIn( user, account, profile) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('users_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email }}
+            ),
+            q.Get(
+              q.Match(
+                q.Index('users_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        )
+
+        return true;
+
+      } catch (err) {
+        console.log(err);
+        return false;
+
+      }
+    }
+  }
 })
