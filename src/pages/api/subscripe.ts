0a0bb2bb29d9afbba6a94e1147b2bdae285ee2f5
Kael Henrique da Silva <kael_hs@hotmail.com>
2021-05-31 20:29:08 -0300

    Stripe subscription API route

---
+++ src/pages/api/subscripe.ts
@@ -0,0 +1,35 @@
+import {NextApiRequest, NextApiResponse } from "next";
+import { stripe } from '../../services/stripe';
+import { getSession } from 'next-auth/client'
+
+export default async ( req: NextApiRequest, res: NextApiResponse) => {
+    if (req.method === 'POST'){
+
+        const session = await getSession({ req });
+
+        const stripeCustomer = await stripe.customers.create({
+            email: session.user.email,
+            //metadata
+        })
+        
+        const stripeCheckoutSession = await stripe.checkout.sessions.create({
+            customer: stripeCustomer.id,
+            payment_method_types: ['card'],
+            billing_address_collection: 'required',
+            line_items: [
+                {price: 'price_1Iw5CrILyd2JYByDn7gn6alC', quantity: 1 }
+            ],
+            mode: 'subscription',
+            allow_promotion_codes: true,
+            success_url: process.env.STRIPE_SUCCESS_URL,
+            cancel_url: process.env.STRIPE_CANCEL_URL
+        })
+
+        return res.status(200).json({sessionId: stripeCheckoutSession.id})
+
+    } else {
+        res.setHeader ('Alow', 'POST');
+        res.status(405).end('Method not allowed');
+    }
+
+}
