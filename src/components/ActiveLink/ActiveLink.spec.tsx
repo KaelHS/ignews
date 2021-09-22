109df7f86a933ff795e7ecd44a7397e807cf07c5
Kael Silva <kael_hs@hotmail.com>
2021-09-22 11:44:03 -0300

    Testing ActiveLink component

---
+++ src/components/ActiveLink/ActiveLink.spec.tsx
@@ -0,0 +1,35 @@
+import { render } from '@testing-library/react';
+import { ActiveLink } from '.';
+
+jest.mock('next/router', () => {
+    return{
+        useRouter() {
+            return {
+                asPath: '/'
+            }
+        }
+    }
+})
+
+describe('ActiveLink component', () => {
+    test('active link renders correctly', () => {
+        const { getByText } = render(
+            <ActiveLink href="/" activeClassName="active">
+                <a>Home</a>
+            </ActiveLink>
+        )
+    
+        expect(getByText('Home')).toBeInTheDocument()
+    })
+    
+    test('active link is receiving active class', () => {
+        const { getByText } = render(
+            <ActiveLink href="/" activeClassName="active">
+                <a>Home</a>
+            </ActiveLink>
+        )
+    
+        expect(getByText('Home')).toHaveClass('active')
+    })
+})
+
